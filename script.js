c9da58ac5a0f11ed587232ec5de9d7800aa9ab05
Farel Lukas <farellukas@gmail.com>
2022-06-24 15:01:08 -0600

    Made rps UI.

--- script.js
+++ script.js
@@ -2,10 +2,53 @@
 
 const options = ['Rock', 'Paper', 'Scissors']
 
+let gameOver = false;
+
+const selections = document.querySelectorAll('.selection');
+selections.forEach((selection) => {
+    selection.addEventListener('click', () => {
+        if (!gameOver) {
+            playRound(selection.dataset.choice, computerPlay());
+        };
+    });
+});
+
 function computerPlay() {
     let choice = Math.floor(Math.random() * 3);
     return options[choice];
-}
+};
+
+function updateScore(winner) {
+    const playerScore = document.querySelector('#playerScore');
+    const computerScore = document.querySelector('#computerScore');
+
+    let playerValue = playerScore.textContent.split(": ");
+    let computerValue = computerScore.textContent.split(": ");
+
+    if (winner === true) {
+        playerValue[1]++;
+    }
+    else {
+        computerValue[1]++;
+    }
+
+    playerScore.textContent = playerValue.join(": ");
+    computerScore.textContent = computerValue.join(": ");
+
+    if (playerValue[1] >= 5 || computerValue[1] >= 5) {
+        gameOver = true;
+        const gameOverLabel = document.createElement('h3');
+        gameOverLabel.innerText = "Game Over";
+        
+        const scores = document.querySelector('.scores');
+        scores.appendChild(gameOverLabel);
+    };
+
+    return {
+        player: playerValue[1],
+        computer: computerValue[1]
+    };
+};
 
 function playRound(playerSelection, computerSelection) {
     // Initialize selections
@@ -49,45 +92,51 @@ function playRound(playerSelection, computerSelection) {
         else {
             result = `You lose! ${playerSelection} loses to ${computerSelection}.`;
         }
+        updateScore(winner);
     }
     else {
         result = `It's a tie! You both chose ${playerSelection}.`;
     }
 
+    const results = document.querySelector(".results");
+    const display = document.createElement('p');
+    display.textContent = result;
+    results.appendChild(display);
+
     return result;
 }
 
-function game() {
-    let playerCounter = 0;
-    let computerCounter = 0;
+// function game() {
+//     let playerCounter = 0;
+//     let computerCounter = 0;
 
-    for (let i=0; i < 5; i++) {
-        // console.log(`Player: ${playerCounter}\nComputer: ${computerCounter}`)
+//     for (let i=0; i < 5; i++) {
+//         // console.log(`Player: ${playerCounter}\nComputer: ${computerCounter}`)
 
-        let playerSelection = prompt("Enter you selection of Rock, Paper, or Scissors:");
-        let computerSelection = computerPlay();
+//         let playerSelection = prompt("Enter you selection of Rock, Paper, or Scissors:");
+//         let computerSelection = computerPlay();
         
-        roundResult = playRound(playerSelection, computerSelection);
-        console.log(roundResult);
+//         roundResult = playRound(playerSelection, computerSelection);
+//         console.log(roundResult);
 
-        if (roundResult.includes("win")) {
-            playerCounter++;
-        }
-        else if (roundResult.includes("lose")) {
-            computerCounter++;
-        }
-    }
+//         if (roundResult.includes("win")) {
+//             playerCounter++;
+//         }
+//         else if (roundResult.includes("lose")) {
+//             computerCounter++;
+//         }
+//     }
 
-    console.log("Final results!");
-    console.log(`Player: ${playerCounter}\nComputer: ${computerCounter}`);
+//     console.log("Final results!");
+//     console.log(`Player: ${playerCounter}\nComputer: ${computerCounter}`);
 
-    if (playerCounter > computerCounter) {
-        console.log("You win!");
-    }
-    else if (playerCounter < computerCounter) {
-        console.log("You lose!");
-    }
-    else {
-        console.log("It's a tie!");
-    }
-}
+//     if (playerCounter > computerCounter) {
+//         console.log("You win!");
+//     }
+//     else if (playerCounter < computerCounter) {
+//         console.log("You lose!");
+//     }
+//     else {
+//         console.log("It's a tie!");
+//     }
+// }
